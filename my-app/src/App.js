64007fc21e171d49852dbd9ee1322f4e3d24b02d
AlexanderJWard <alexjamesward@hotmail.com>
2023-03-07 16:28:21 +0000

    Add content hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,14 @@ import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
 import PostItem from './components/PostItem';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
     <div>
       <NavBarForm/>
       <Sidebar/>
-      <Content/>
+      <ContentHooks/>
       <PostItem/>
     </div>
   );
